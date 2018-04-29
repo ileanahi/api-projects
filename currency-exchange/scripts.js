81a0f3a118f376fce2e688bbba51ec60ab71930c
Ileana Argyris <argyris.ileana@gmail.com>
2018-04-28 21:17:34 -1000

    Test

--- currency-exchange/scripts.js
+++ currency-exchange/scripts.js
@@ -3,9 +3,9 @@ var request = new XMLHttpRequest();
 request.open("GET", "https://exchangeratesapi.io/api/latest?base=USD", true);
 
 request.onload = function() {
-var data = JSON.parse(this.response);
-console.log(data.rates.AUD);
-});
+    var data = JSON.parse(this.response);
+    console.log(data.rates.AUD);
+};
 
 // Send request
 request.send();
