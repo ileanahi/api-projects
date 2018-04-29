730822b0d9ad3e35a31e2abf82736d1229b81b6a
Ileana Argyris <argyris.ileana@gmail.com>
2018-04-28 21:38:32 -1000

    Test

--- currency-exchange/scripts.js
+++ currency-exchange/scripts.js
@@ -4,9 +4,8 @@ request.open("GET", "https://exchangeratesapi.io/api/latest?base=USD", true);
 
 request.onload = function() {
         var data = JSON.parse(this.response);
-        Object.keys(data).forEach(function(key) {
-            console.log(data[key]);
-        });
+
+        console.log(Object.keys(data));
 
         // Send request
         request.send();
