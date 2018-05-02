0e2ae45d7228b9c8305ce11ca5543be949b9f10e
Ileana Argyris <argyris.ileana@gmail.com>
2018-05-01 14:42:22 -1000

    Update CSS

--- currency-exchange/scripts.js
+++ currency-exchange/scripts.js
@@ -3,9 +3,22 @@ var request = new XMLHttpRequest();
 request.open("GET", "https://exchangeratesapi.io/api/latest?base=USD", true);
 
 request.onload = function() {
-        var data = JSON.parse(this.response);
+    if (request.status >= 200 && request.status < 400) {
+        var data = JSON.parse(this.responseText);
+        if (typeof fx !== "undefined" && fx.rates) {
+            fx.rates = data.rates;
+            fx.base = data.base;
+        } else {
+            var fxSetup = {
+                rates: data.rates,
+                base: data.base
+            }
+        }
+    } else {
+        console.log("Oops! An error.");
+    }
+};
 
-        console.log(Object.keys(data[rates][AUD]);
 
-                // Send request
-                request.send();
+// Send request
+request.send();
